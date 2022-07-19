50c04085e4389b8ba0e07a4ecdce9dc66157ab8c
LoiTran <tranloi781@gmail.com>
2022-07-19 15:15:27 -0500

    changing port command

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const app = express();
+const PORT = process.env.PORT || 3000;
 
 const { notes } = require("./db/db.json");
 
@@ -8,9 +8,9 @@ app.get("/api/notes", (req, res) => {
   res.json(note);
 });
 
-app.listen(3000, () => {
-  console.log(`API server now on port 3000!`);
-});
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
 
 function filterByQuery(query, notesArray) {
   let filteredResults = notesArray;
